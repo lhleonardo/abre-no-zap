0f35b0f4668c4a86933a0d0ae04780290c2a23aa
Leonardo <lhleonardo05@gmail.com>
2021-03-02 11:43:09 -0300

    Initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+function Home() {
+    return <Container>
+        <Text>Home Page</Text>
+    </Container>
+}
+
+
+export { Home };
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`;
